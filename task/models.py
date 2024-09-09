863f8cb8cbffcf9cbc20cad2fcadc22a2f126d52
jazzmatic-jazz <jazminemazzey@gmail.com>
2024-09-09 17:15:28 +0530

    Add: Task Model

--- task/models.py
+++ task/models.py
@@ -29,4 +29,12 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
     
 
+class Tasks(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_tasks')
+    title = models.CharField(max_length=100, blank=False, null=False)
+    description = models.CharField(max_length=350, null=True, blank=True)
+    due_date = models.DateField(auto_now=False, auto_now_add=False, editable=True)
+    is_completed = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.title
